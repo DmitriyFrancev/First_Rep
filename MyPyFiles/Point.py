3f0dce797788628e899b0601e8ee2db6eb65126c
Dmitriy <DmitriyFrancev@mail.ru>
2022-07-10 19:06:36 +0300

    Добавлены файлы к лекциям по классам

--- MyPyFiles/Point.py
+++ MyPyFiles/Point.py
@@ -13,3 +13,11 @@ class Point:
     def distance_to(self, point):
         return ((self.x - point.x)**2 + (self.y - point.y)**2)**0.5
     
+    def __str__(self):      # Определяем строковое представление объекта
+        class_info = 'Point({0:.5f}, {1:.5f})'.format(self.x, self.y)   # 'Point({0:.5f}, {1:.5f})' - форматирование строки вывода (до 5 знаков для каждого аргумента)
+        return class_info
+
+class Point3D(Point):
+    def __init__ (self, x, y, z):
+        Point.__init__(self, x, y)   # Инициализация свойств родителя
+        self.z = z                   # Инициализация дополнительного свойства потомка
--- MyPyFiles/Test_point_object.py
+++ MyPyFiles/Test_point_object.py
@@ -1,13 +1,18 @@
-from Point import Point
+from point import Point, Point3D
 
 point1 = Point(10, 15)
 point2 = Point(15, 22)
 
-print(point1.x, point1.y)
-print(point2.x, point2.y)
+# print(point1.x, point1.y)
+# print(point2.x, point2.y)
 
-print(point1.distance_to(point2))
-print(point2.distance_to(point2))
+# print(point1.distance_to(point2))
+# print(point2.distance_to(point2))
 
 print(point1)
-print(type(point1))
+# print(type(point1))
+
+point3D_1 = Point3D(2,4,5)
+point3D_2 = Point3D(15,14,25)
+
+print(point3D_1.x)
